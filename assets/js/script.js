38730833d6654a0d98e38ef5175a77284cc3629b
mflanigan <mflanigantwualumn@gmail.com>
2022-01-04 15:55:50 -0600

    added clear buttons to book elements and started writing js function for local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,17 @@
 var searchBtn = document.querySelector("#search-btn");
 var resultEl = document.querySelector("#search-results");
 var myBookEl = document.querySelector("#my-book-container");
+var currentReadEl = document.querySelector(".currently-reading");
+var toReadEl = document.querySelector(".to-read-list");
+var readEl = document.querySelector(".read-list");
 
 // function to display items from search bar
-var displaySearch = async function(event) {
+var displaySearch = async function (event) {
     event.preventDefault();
     var searchTerm = document.querySelector("#title").value;
     searchTerm = searchTerm.split(" ").join("-");
-    var apiKey = "AIzaSyAH_XNq1-MK1My5OziQ3WIbWCh4WQPxPV8"
-    var endPoint = `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}`
+    var apiKey = "AIzaSyAH_XNq1-MK1My5OziQ3WIbWCh4WQPxPV8";
+    var endPoint = `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}`;
 
     // change classes to display correct div
     myBookEl.setAttribute("class", "hide");
@@ -19,17 +22,17 @@ var displaySearch = async function(event) {
     // part of the async await to configue promise chain
     const response = await fetch(endPoint);
 
-    if(!response.ok) {
-        throw new Error("something went wrong")
-    };
+    if (!response.ok) {
+        throw new Error("something went wrong");
+    }
 
     const { items } = await response.json();
     console.log(items);
 
     // for each item in array create a card to display content
-    items.forEach(function(item) {
-        var title = item.volumeInfo.title
-        var image = item.volumeInfo.imageLinks.thumbnail 
+    items.forEach(function (item) {
+        var title = item.volumeInfo.title;
+        var image = item.volumeInfo.imageLinks.thumbnail;
 
         var titleEl = document.createElement("h3");
         var imageEl = document.createElement("img");
@@ -48,60 +51,60 @@ var displaySearch = async function(event) {
 
         // add buttons to the card for currently reading
         currentRead.setAttribute("class", "button");
-        currentRead.addEventListener("click", addtoCurrentRead)
-        currentRead.textContent = "Currently Reading"
-        
+        currentRead.addEventListener("click", addtoCurrentRead);
+        currentRead.textContent = "Currently Reading";
+
         // add buttons to the card for to read
         toRead.setAttribute("class", "button");
-        toRead.addEventListener("click", addToRead)
-        toRead.textContent = "To Read"
-        
+        toRead.addEventListener("click", addToRead);
+        toRead.textContent = "To Read";
+
         // add buttons to the card for read
         read.setAttribute("class", "button");
-        read.addEventListener("click", addRead)
-        read.textContent = "Read Book"
-        
-        titleEl.textContent = title 
-        imageEl.setAttribute("src", image)
-        
+        read.addEventListener("click", addRead);
+        read.textContent = "Read Book";
+
+        titleEl.textContent = title;
+        imageEl.setAttribute("src", image);
+
         // append to elements and cards
         cardTitleEl.appendChild(titleEl);
         cardImgEl.appendChild(imageEl);
-        
+
         card.appendChild(cardImgEl);
         card.appendChild(cardTitleEl);
-        
+
         card.appendChild(currentRead);
         card.appendChild(toRead);
         card.appendChild(read);
-        
+
         currentRead.info = {
-            title, image 
-        }
+            title,
+            image,
+        };
 
         resultEl.appendChild(card);
-        
-        
-
     });
-
 };
 
 // functions to save to local storage
 function addtoCurrentRead(event) {
     console.log(event.currentTarget.info);
-};
 
-function addToRead(event) {
-
-};
-
-function addRead(event) {
     
-};
 
+    localStorage.setItem("title", "title");
+    localStorage.getItem("title");
+
+    
+}
+
+function addToRead(event) { }
+
+function addRead(event) { }
+
+// function to clear each book container
 
 // event listenters
 searchBtn.addEventListener("click", displaySearch);
-
-
+currentRead.addEventListener("click", addtoCurrentRead);
