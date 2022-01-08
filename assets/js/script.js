f8f32b8792322a74b5d8b83fa7924d198cc3e0df
mflanigan <mflanigantwualumn@gmail.com>
2022-01-08 12:29:41 -0600

    got onload function working to display info from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -92,6 +92,16 @@ var displaySearch = async function (event) {
             image,
         };
 
+        toRead.info = {
+            title,
+            image,
+        };
+
+        read.info = {
+            title,
+            image,
+        };
+
         resultEl.appendChild(card);
     });
 };
@@ -109,105 +119,147 @@ function addToRead(event) {
     console.log(event.currentTarget.info);
     var volume2 = event.currentTarget.info;
     localStorage.setItem("volume2", JSON.stringify(volume2));
-    localStorage.getItem("volume2");
 };
-
 
 function addRead(event) {
     console.log(event.currentTarget.info);
     var volume3 = event.currentTarget.info;
     localStorage.setItem("volume3", JSON.stringify(volume3));
-    localStorage.getItem("volume3");
 };
 
-// function to display information in local storage to mybook page
+// function to display information in local storage to currently reading
 window.addEventListener('load',
     function () {
         var volume1data = JSON.parse(localStorage.getItem("volume1"));
-        console.log(volume1data);
-        
 
-        volume1data.forEach(function (currentReadEl) {
-            var title = item.volumeInfo.title;
-            var image = item.volumeInfo.imageLinks.thumbnail;
+        var title = volume1data.title;
+        var image = volume1data.image;
 
-            var titleEl = document.createElement("h3");
-            var imageEl = document.createElement("img");
-            var card = document.createElement("div");
+        var titleEl = document.createElement("h3");
+        var imageEl = document.createElement("img");
+        var card = document.createElement("div");
 
-            var cardTitleEl = document.createElement("div");
-            var cardImgEl = document.createElement("div");
+        var cardTitleEl = document.createElement("div");
+        var cardImgEl = document.createElement("div");
 
-            card.setAttribute("class", "card");
-            cardTitleEl.setAttribute("class", "card-content");
-            cardImgEl.setAttribute("class", "card-image");
+        card.setAttribute("class", "card");
+        cardTitleEl.setAttribute("class", "card-content");
+        cardImgEl.setAttribute("class", "card-image");
 
-            titleEl.textContent = title;
-            imageEl.setAttribute("src", image);
+        titleEl.textContent = title;
+        imageEl.setAttribute("src", image);
 
-            cardTitleEl.appendChild(titleEl);
-            cardImgEl.appendChild(imageEl);
+        cardTitleEl.appendChild(titleEl);
+        cardImgEl.appendChild(imageEl);
 
-            card.appendChild(cardImgEl);
-            card.appendChild(cardTitleEl);
+        card.appendChild(cardImgEl);
+        card.appendChild(cardTitleEl);
 
-            currentReadEl.appendChild(card);
+        currentReadEl.appendChild(card);
+    }
+);
 
-        })
+// function to display information in local storage to 
+window.addEventListener('load',
+    function () {
+        var volume2data = JSON.parse(localStorage.getItem("volume2"));
+
+        var title = volume2data.title;
+        var image = volume2data.image;
+
+        var titleEl = document.createElement("h3");
+        var imageEl = document.createElement("img");
+        var card = document.createElement("div");
+
+        var cardTitleEl = document.createElement("div");
+        var cardImgEl = document.createElement("div");
+
+        card.setAttribute("class", "card");
+        cardTitleEl.setAttribute("class", "card-content");
+        cardImgEl.setAttribute("class", "card-image");
+
+        titleEl.textContent = title;
+        imageEl.setAttribute("src", image);
+
+        cardTitleEl.appendChild(titleEl);
+        cardImgEl.appendChild(imageEl);
+
+        card.appendChild(cardImgEl);
+        card.appendChild(cardTitleEl);
+
+        toReadEl.appendChild(card);
+    }
+);
+
+window.addEventListener('load',
+    function () {
+        var volume3data = JSON.parse(localStorage.getItem("volume2"));
+
+        var title = volume3data.title;
+        var image = volume3data.image;
+
+        var titleEl = document.createElement("h3");
+        var imageEl = document.createElement("img");
+        var card = document.createElement("div");
+
+        var cardTitleEl = document.createElement("div");
+        var cardImgEl = document.createElement("div");
+
+        card.setAttribute("class", "card");
+        cardTitleEl.setAttribute("class", "card-content");
+        cardImgEl.setAttribute("class", "card-image");
+
+        titleEl.textContent = title;
+        imageEl.setAttribute("src", image);
+
+        cardTitleEl.appendChild(titleEl);
+        cardImgEl.appendChild(imageEl);
+
+        card.appendChild(cardImgEl);
+        card.appendChild(cardTitleEl);
+
+        readEl.appendChild(card);
     }
 );
 
 
-// // function to display information in local storage to divs on my-books page
-// var localStorageDisplay = function() {
+// function to display Best Seller Category on Homepage
+var displayBestSellers = async function () {
+    var NYTurl = `https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json${NYTAPIKEY}`;
 
-//     console.log("it works");
+    fetch(NYTurl)
+        .then(response => response.json())
+        .then((data) => {
+            var bestsellersList = data.results
+            for (let i = 0; i < 10; i++) {
+                let booktitle = bestsellersList[i].title;
+                let bookAuthor = bestsellersList[i].author;
+                var listItem = document.createElement('li');
+                listItem.textContent = `${booktitle} by ${bookAuthor}`;
+                bestsellersEl.appendChild(listItem)
+            } 
+        });
+};
 
-//     if(localStorage === 0) {
-//         currentReadEl.textContent = "No books saved yet!";
-//         return;
-//     }
-// };
+// function to display Review search on Homepage
+var displayReviews =  async function (event) {
+    event.preventDefault();
 
+    var reviewSearch = reviewsInput.value.trim();
+    console.log(reviewSearch)
 
-// var displayBestSellers = async function () {
-//     var NYTurl = `https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json${NYTAPIKEY}`;
+    var reviewsURL = `https://api.nytimes.com/svc/books/v3/reviews.json${NYTAPIKEY}`;
 
-//     fetch(NYTurl)
-//         .then(response => response.json())
-//         .then((data) => {
-//             var bestsellersList = data.results
-//             for (let i = 0; i < 10; i++) {
-//                 let booktitle = bestsellersList[i].title;
-//                 let bookAuthor = bestsellersList[i].author;
-//                 var listItem = document.createElement('li');
-//                 listItem.textContent = `${booktitle} by ${bookAuthor}`;
-//                 bestsellersEl.appendChild(listItem)
-//             } 
-//         });
-// };
-
-
-// var displayReviews =  async function (event) {
-//     event.preventDefault();
-
-//     var reviewSearch = reviewsInput.value.trim();
-//     console.log(reviewSearch)
-
-//     var reviewsURL = `https://api.nytimes.com/svc/books/v3/reviews.json${NYTAPIKEY}`;
-
-//     fetch(reviewsURL)
-//         .then(response => response.json())
-//         .then((data) => {
-//             console.log(data)
-//         });
-// };
+    fetch(reviewsURL)
+        .then(response => response.json())
+        .then((data) => {
+            console.log(data)
+        });
+};
 
 
-// displayBestSellers();
-// reviewsBtnEl.addEventListener('click', displayReviews)
-
-
+displayBestSellers();
+reviewsBtnEl.addEventListener('click', displayReviews)
 
 
 // function to clear each book container
